c90f4f83c0308c88675b296bebd80c8b95ff3f5f
fsegurai <fabiansegu@gmail.com>
2024-11-15 14:18:22 -0600

    refactor/LowPowerPtc event handler

--- src/Ryujinx.UI.Common/Configuration/ConfigurationState.Model.cs
+++ src/Ryujinx.UI.Common/Configuration/ConfigurationState.Model.cs
@@ -1,4 +1,5 @@
-﻿using Ryujinx.Common;
+﻿using ARMeilleure;
+using Ryujinx.Common;
 using Ryujinx.Common.Configuration;
 using Ryujinx.Common.Configuration.Hid;
 using Ryujinx.Common.Configuration.Multiplayer;
@@ -376,6 +377,7 @@ namespace Ryujinx.UI.Common.Configuration
                 EnablePtc.LogChangesToValue(nameof(EnablePtc));
                 EnableLowPowerPtc = new ReactiveObject<bool>();
                 EnableLowPowerPtc.LogChangesToValue(nameof(EnableLowPowerPtc));
+                EnableLowPowerPtc.Event += (_, evnt) => Optimizations.LowPower = evnt.NewValue;
                 EnableInternetAccess = new ReactiveObject<bool>();
                 EnableInternetAccess.LogChangesToValue(nameof(EnableInternetAccess));
                 EnableFsIntegrityChecks = new ReactiveObject<bool>();
