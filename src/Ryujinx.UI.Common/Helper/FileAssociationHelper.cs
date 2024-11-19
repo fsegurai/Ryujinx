6662987a49318045ee9e9d91d2fee3d7054c1a24
fsegurai <fabiansegu@gmail.com>
2024-11-19 12:46:20 -0600

    fix/nullref

--- src/Ryujinx.UI.Common/Helper/FileAssociationHelper.cs
+++ src/Ryujinx.UI.Common/Helper/FileAssociationHelper.cs
@@ -14,8 +14,8 @@ namespace Ryujinx.UI.Common.Helper
     {
         private static readonly string[] _fileExtensions = [".nca", ".nro", ".nso", ".nsp", ".xci"];
 
-        [SupportedOSPlatform("linux")]
-        private static readonly string _mimeDbPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), ".local", "share", "mime");
+        [SupportedOSPlatform("linux")] private static readonly string _mimeDbPath =
+            Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), ".local", "share", "mime");
 
         private const int SHCNE_ASSOCCHANGED = 0x8000000;
         private const int SHCNF_FLUSH = 0x1000;
@@ -23,8 +23,9 @@ namespace Ryujinx.UI.Common.Helper
         [LibraryImport("shell32.dll", SetLastError = true)]
         public static partial void SHChangeNotify(uint wEventId, uint uFlags, nint dwItem1, nint dwItem2);
 
-        public static bool IsTypeAssociationSupported => (OperatingSystem.IsLinux() || OperatingSystem.IsWindows()) && !ReleaseInformation.IsFlatHubBuild;
-        
+        public static bool IsTypeAssociationSupported => (OperatingSystem.IsLinux() || OperatingSystem.IsWindows()) &&
+                                                         !ReleaseInformation.IsFlatHubBuild;
+
         public static bool AreMimeTypesRegistered
         {
             get
@@ -46,7 +47,8 @@ namespace Ryujinx.UI.Common.Helper
         }
 
         [SupportedOSPlatform("linux")]
-        private static bool AreMimeTypesRegisteredLinux() => File.Exists(Path.Combine(_mimeDbPath, "packages", "Ryujinx.xml"));
+        private static bool AreMimeTypesRegisteredLinux() =>
+            File.Exists(Path.Combine(_mimeDbPath, "packages", "Ryujinx.xml"));
 
         [SupportedOSPlatform("linux")]
         private static bool InstallLinuxMimeTypes(bool uninstall = false)
@@ -68,7 +70,8 @@ namespace Ryujinx.UI.Common.Helper
 
                 if (mimeProcess.ExitCode != 0)
                 {
-                    Logger.Error?.PrintMsg(LogClass.Application, $"Unable to {installKeyword} mime types. Make sure xdg-utils is installed. Process exited with code: {mimeProcess.ExitCode}");
+                    Logger.Error?.PrintMsg(LogClass.Application,
+                        $"Unable to {installKeyword} mime types. Make sure xdg-utils is installed. Process exited with code: {mimeProcess.ExitCode}");
 
                     return false;
                 }
@@ -83,7 +86,8 @@ namespace Ryujinx.UI.Common.Helper
 
                 if (updateMimeProcess.ExitCode != 0)
                 {
-                    Logger.Error?.PrintMsg(LogClass.Application, $"Could not update local mime database. Process exited with code: {updateMimeProcess.ExitCode}");
+                    Logger.Error?.PrintMsg(LogClass.Application,
+                        $"Could not update local mime database. Process exited with code: {updateMimeProcess.ExitCode}");
                 }
             }
 
@@ -93,31 +97,29 @@ namespace Ryujinx.UI.Common.Helper
         [SupportedOSPlatform("windows")]
         private static bool AreMimeTypesRegisteredWindows()
         {
-            return _fileExtensions.Aggregate(false, 
+            return _fileExtensions.Aggregate(false,
                 (current, ext) => current | CheckRegistering(ext)
             );
-            
+
             static bool CheckRegistering(string ext)
             {
                 RegistryKey key = Registry.CurrentUser.OpenSubKey(@$"Software\Classes\{ext}");
 
-                if (key is null)
-                {
-                    return false;
-                }
+                var openCmd = key?.OpenSubKey(@"shell\open\command");
 
-                var openCmd = key.OpenSubKey(@"shell\open\command");
+                if (openCmd is null) return false;
 
                 string keyValue = (string)openCmd.GetValue(string.Empty);
 
-                return keyValue is not null && (keyValue.Contains("Ryujinx") || keyValue.Contains(AppDomain.CurrentDomain.FriendlyName));
+                return keyValue is not null && (keyValue.Contains("Ryujinx") ||
+                                                keyValue.Contains(AppDomain.CurrentDomain.FriendlyName));
             }
         }
 
         [SupportedOSPlatform("windows")]
         private static bool InstallWindowsMimeTypes(bool uninstall = false)
         {
-            bool registered = _fileExtensions.Aggregate(false, 
+            bool registered = _fileExtensions.Aggregate(false,
                 (current, ext) => current | RegisterExtension(ext, uninstall)
             );
 
@@ -125,7 +127,7 @@ namespace Ryujinx.UI.Common.Helper
             SHChangeNotify(SHCNE_ASSOCCHANGED, SHCNF_FLUSH, nint.Zero, nint.Zero);
 
             return registered;
-            
+
             static bool RegisterExtension(string ext, bool uninstall = false)
             {
                 string keyString = @$"Software\Classes\{ext}";
@@ -137,6 +139,7 @@ namespace Ryujinx.UI.Common.Helper
                     {
                         return true;
                     }
+
                     Logger.Debug?.Print(LogClass.Application, $"Removing type association {ext}");
                     Registry.CurrentUser.DeleteSubKeyTree(keyString);
                     Logger.Debug?.Print(LogClass.Application, $"Removed type association {ext}");
@@ -154,7 +157,6 @@ namespace Ryujinx.UI.Common.Helper
                     using var openCmd = key.CreateSubKey(@"shell\open\command");
                     openCmd.SetValue(string.Empty, $"\"{Environment.ProcessPath}\" \"%1\"");
                     Logger.Debug?.Print(LogClass.Application, $"Added type association {ext}");
-
                 }
 
                 return true;
