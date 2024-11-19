42fe0583670b4f0337d02e549ef8b207adf95113
fsegurai <fabiansegu@gmail.com>
2024-11-18 20:01:45 -0600

    fix/test package version

--- src/Ryujinx.Tests.Memory/TrackingTests.cs
+++ src/Ryujinx.Tests.Memory/TrackingTests.cs
@@ -191,7 +191,7 @@ namespace Ryujinx.Tests.Memory
             Assert.That(alignedAfterTriggers, Is.False);
         }
 
-        [Test, Explicit, CancelAfter(1000)]
+        [Test, Explicit, Timeout(1000)]
         public void Multithreading()
         {
             // Multithreading sanity test
